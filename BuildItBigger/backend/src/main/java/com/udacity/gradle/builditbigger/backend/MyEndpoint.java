d9f53218d000198b4f05030752618cc11a332d22
cruz.camilo <ccamilo_eduardo@hotmail.com>
2018-11-29 22:19:38 -0500

    feat: Add AsyncTask class.
    AsyncTask retrieves a joke from GCE

--- BuildItBigger/backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ BuildItBigger/backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -5,8 +5,6 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 
-import javax.inject.Named;
-
 /** An endpoint class we are exposing */
 @Api(
         name = "myApi",
@@ -20,10 +18,10 @@ import javax.inject.Named;
 public class MyEndpoint {
 
     /**
-     * A simple endpoint method that takes a name and says Hi back
+     * This Method uses java class "Jokes" from Java library "sillyJokes" to return a joke.
      */
     @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    public MyBean sayHi() {
         MyBean response = new MyBean();
         response.setData(new Jokes().getJoke());
         return response;
